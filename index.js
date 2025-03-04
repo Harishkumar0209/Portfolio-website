6a1c25512b633888b9d7de2b0dc425bc2052199c
Harishkumar0209 <Ksree1047@gmail.com>
2025-03-04 17:13:06 -0500

    portfolio

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    Strings:["Frontend Developer" , "Web Developer", "Digital Marketer", "Content Writer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    looped:true
+});
+
