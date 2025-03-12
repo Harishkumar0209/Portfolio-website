248c95e03971a984552ec807113f3f0c544dcc28
Harishkumar0209 <Ksree1047@gmail.com>
2025-03-12 12:06:03 -0400

    new-change

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const menu = document.querySelector('.menu')
+const menuList = document.querySelector('nav ul')
+menu.addEventListener('click',()=>{
+    menuList.classList.toggle('showmenu')
+})
+
 var typed = new Typed(".text", {
     Strings:["Frontend Developer" , "Web Developer", "Digital Marketer", "Content Writer"],
     typeSpeed:100,
