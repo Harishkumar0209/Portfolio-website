e79791315f3679368f5d92703ccd1d32ccad5184
Harishkumar0209 <Ksree1047@gmail.com>
2025-05-21 15:30:39 -0400

    21/5

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const menuList = document.querySelector('nav ul')
 menu.addEventListener('click',()=>{
     menuList.classList.toggle('showmenu')
 })
-
-
+ 
 
 function show() {
     alert("Thanks! for visit my page 'your message has been send'");
